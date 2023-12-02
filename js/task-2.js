bfef02118068cdd0df68fdbcad4cce41ce3373b8
Skipalsky <142356203+Skipalsky@users.noreply.github.com>
2023-12-03 00:08:28 +0200

    task 2-3

---
+++ js/task-2.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class Storage{
+
+    #items;
+
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        /*
+        const index = this.#items.indexOf(itemToRemove);
+        this.#items.splice(index, index);
+        */
+        this.#items = this.#items.filter((item) => item !== itemToRemove);
+    }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
